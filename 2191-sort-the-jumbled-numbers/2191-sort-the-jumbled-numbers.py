2b46edc26bf5ff75a1563ef21760b34c3030c3fb
geethvishnu <107038682+geethvishnu@users.noreply.github.com>
2024-07-24 18:22:26 +0530

    Time: 1252 ms (17.39%), Space: 26.5 MB (22.28%) - LeetHub

--- 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.py
+++ 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.py
@@ -3,32 +3,36 @@ class Solution:
     def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
         
             # l=[]
-            # for i in nums:
+            # for q,i in enumerate(nums):
             #     a=i 
             #     digit=0
                 
-            #     # place=1
-            #     while a>0:
+            #     place=1
+            #     while a:
             #         rem=a%10
-            #         digit=digit*10+mapping[rem]
+            #         digit=digit+mapping[rem]*place
             #         a//=10
-            #     digit=str(digit)
-            #     digit=int(digit[::-1])
-            #     l.append(digit)
+            #         place*=10
+            #     # digit=str(digit)
+            #     # digit=int(digit[::-1])
+            #     l.append((digit,q))
             # print(l)
-            # d={}
-            # for i in range(len(nums)):
-            #     d[nums[i]]=l[i]
-            # d1=dict(sorted(d.items(),key=lambda x:x[1]))
-            # print(d1)
-            # b=[]
-            # for i,j in d1.items():
-            #     b.append(i)
-            # print(b)
+            # l.sort()
+            # # return 
+            # return [nums[p[1]] for p in l]
+            # # d={}
+            # # for i in range(len(nums)):
+            # #     d[nums[i]]=l[i]
+            # # d1=dict(sorted(d.items(),key=lambda x:x[1]))
+            # # print(d1)
+            # # b=[]
+            # # for i,j in d1.items():
+            # #     b.append(i)
+            # # print(b)
 
-            # if 0 in nums:
-            #     b.pop(0)
-            #     b.append(0)
+            # # if 0 in nums:
+            # #     b.pop(0)
+            # #     b.append(0)
             # # # count=Counter(l)
             # # # sortedlist=sorted(l,key=lambda x:(count[x],x))
             # # sortedlist=sorted(nums,key=lambda x:(d[x],x))
@@ -40,7 +44,7 @@ class Solution:
             # #             nums[j],nums[j+1]=nums[j+1],nums[j]
 
 
-            # return b
+            # # return []
 
             l=[]
             for i, n in enumerate(nums):
